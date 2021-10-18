eda63adb86256bee4edad615c11c5b93f6d56ba7
Manali Chahande <92083877+risertechub@users.noreply.github.com>
2021-10-18 13:38:28 +0530

    Add files via upload

--- api/model/marks.js
+++ api/model/marks.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose')
 const marksSchema = new mongoose.Schema({
     
     _id: mongoose.Schema.Types.ObjectId, 
-    studentId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Student',
-        required: true,
+    // studentId:{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Student',
+    //     required: true,
+    // },
+    fname : {
+        type:String,
+        ref:'Student'
     },
     english: {
         type: Number,
--- api/routes/marks.js
+++ api/routes/marks.js
@@ -5,123 +5,42 @@ const Marks = require('../model/marks')
 const Student = require('../model/student')
 
 
-// read only student name nd marks
-// router.get('/name',async(req,res,next) =>{
-//     Marks.aggregate([
-//         {
-//             $lookup: {
-//                 from: "students",
-//                 localField: "_id",
-//                 foreignField: "student",
-//                 as: "student",
-//             },
-//         },            
-//         {   
-//             $unwind: "$student",
-//         },     
-//     ])
-//     .then((result) => {
-//         res.status(200).json({
-//             message:'Student Name and Marks',
-//             result
-//         });
-//         })
-//         .catch((error) => {
-//         console.log(error);
-//         });
-// })
-
-router.get('/greater',async(req,res,next) =>{
+// Get student marks greater than 30
+router.get('/greater',async(req,res,next) => {
     Marks.aggregate([
         {
-            $match : {english: {$gte : 30}, "math":{"$gte":30}, "science":{"$gte":30}}
+            $match :{ "english" : {"$gte":30},"math" : {"$gte":30}, "science" : {"$gte":30}}
         },
-        {
-            $lookup: {
-                from: "students",  
-                as: "Student",
-                let: {studentId: "$_id"},
-                pipeline: [{
-                    $group : {_id: "$fname"} 
-                }]
-            }
-        },   
     ])
     .then((result) => {
-        res.status(200).json({
-            message:'Student Name and Marks',
-            result
-        });
-        })
-        .catch((error) => {
+    res.status(200).json({
+        message:'Student Marks Greater than 30',
+        result
+    });
+    })
+    .catch((error) => {
         console.log(error);
-        });
-})
-
-// read only student name nd marks
-router.get('/name',async(req,res,next) =>{
-    Student.aggregate([
-        {$match : {age: {$gt : 10}}} , {$project : {_id : 1, fname : 1, lname :1, Student :1}},
-        {
-            $lookup: {
-                from: "student_marks",
-                localField: "_id",
-                foreignField: "studentId",
-                as: "Student_Marks",
-            },
-        },       
-        {   
-            $unwind: "$Student_Marks",
-        }, 
-    ])
-    .then((result) => {
-        res.status(200).json({
-            message:'Student Name and Marks',
-            result
-        });
+        res.status(404).json({
+            message :'Marks not found! :',
+            error
         })
-        .catch((error) => {
-        console.log(error);
-        });
+    });
 })
 
 
-// router.get('/greater',async(req,res,next) =>{
-//     Marks.aggregate([
-//         {
-//            $group : {_id:{english : "$english", math: "$math", science : "$science"},
-//             totalMarks : { $sum : "$english", $sum : "$math", $sum : "$science"}}
-//         },
-//         {
-//             $match : {totalMarks : {$gte: 30}}
-//         },
-//     ])
-//     .then((result) => {
-//         res.status(200).json({
-//             message:'Student Marks greather than 30',
-//             result
-//         });
-//         })
-//         .catch((error) => {
-//         console.log(error);
-//         });
-// })
 
-
-
-// Get All Student Marks
+// Get All Students Marks
 router.get('/show',async(req,res,next) => {
     Student.aggregate([
         {
             $lookup: {
                 from: "student_marks",
                 localField: "_id",
-                foreignField: "studentId",
+                foreignField: "_id",
                 as: "Student_Marks",
             },
-        },
-            
-        {   
+        },  
+        {
             $unwind: "$Student_Marks",
         },
     ])
@@ -133,10 +52,12 @@ router.get('/show',async(req,res,next) => {
     })
     .catch((error) => {
     console.log(error);
+    res.status(404).json({
+        message :'Marks not found! :',
+        error
+    })
     });
-  
 })
-
 
 // Get specific student marks using student id
 router.get('/show/:id', async(req, res, next) =>{
@@ -174,8 +95,8 @@ router.post('/insert/:id', async(req, res, next) => {
     try{
             const student = await Student.findById({_id:req.params.id})
             const marks = new Marks({
-            _id:new mongoose.Types.ObjectId,
-            studentId:{_id: student._id,},
+                _id : student._id,
+            fname : student.fname,
             english: req.body.english,
             math: req.body.math,
             science: req.body.science,
@@ -198,8 +119,7 @@ router.post('/insert/:id', async(req, res, next) => {
 // Update student marks using student id
 router.patch('/update/:id',async(req,res,next) => {
     try{
-        const student = await Student.findById({_id:req.params.id})
-        const marks = await Marks.findOne({student}).populate("studentId");
+        const marks = await Marks.findByIdAndUpdate({_id:req.params.id})
         marks.english = req.body.english,
         marks.math = req.body.math,
         marks.science = req.body.science
@@ -210,33 +130,141 @@ router.patch('/update/:id',async(req,res,next) => {
             m1
         })
     }
-        catch(err){
-            console.log(err);
-            res.status(204).json({
-                message : 'Unable to update student marks :',
-                err
-            })
-        }
+    catch(err){
+        console.log(err);
+        res.status(404).json({
+            message : 'Unable to update student marks :',
+            err
+        })
+    }
 })
 
 // Delete student marks using id
 router.delete('/delete/:id', async(req, res, next) => {
     try{
-        const student = await Student.findById({_id:req.params.id})
-        const marks = await Marks.findOne({student}).populate("studentId");
-
-        const m1 = await marks.remove()
+        const marks = await Marks.remove({_id:req.params.id})
         res.status(200).json({
-            message :  'Student Marks Deleted Successfully!'
+            message :  'Student Marks Deleted Successfully!',
+            marks
         })  
     }
     catch(err){
-        res.status(204).json({
+        res.status(404).json({
             message : 'Unable to delete student marks :',
             err
         })
     }
 })
+
+
+
+
+
+
+
+// read only student name nd marks
+// router.get('/name',async(req,res,next) =>{
+//     Marks.aggregate([
+//         {
+//             $lookup: {
+//                 from: "students",
+//                 localField: "_id",
+//                 foreignField: "student",
+//                 as: "student",
+//             },
+//         },            
+//         {   
+//             $unwind: "$student",
+//         },     
+//     ])
+//     .then((result) => {
+//         res.status(200).json({
+//             message:'Student Name and Marks',
+//             result
+//         });
+//         })
+//         .catch((error) => {
+//         console.log(error);
+//         });
+// })
+
+// router.get('/greater',async(req,res,next) =>{
+//     Marks.aggregate([
+//         {
+//             $match : {english: {$gte : 30}, "math":{"$gte":30}, "science":{"$gte":30}}
+//         },
+//         {
+//             $lookup: {
+//                 from: "students",  
+//                 as: "Student",
+//                 pipeline: [{
+//                     $group : {_id: "$fname"} 
+//                     }
+//                 ]
+//             }
+//         },   
+//     ])
+//     .then((result) => {
+//         res.status(200).json({
+//             message:'Student Name and Marks',
+//             result
+//         });
+//         })
+//         .catch((error) => {
+//         console.log(error);
+//         });
+// })
+
+// read only student name nd marks
+// router.get('/name',async(req,res,next) =>{
+//     Student.aggregate([
+//         {$match : {age: {$gt : 10}}} , {$project : {_id : 1, fname : 1, lname :1, Student :1}},
+//         {
+//             $lookup: {
+//                 from: "student_marks",
+//                 localField: "_id",
+//                 foreignField: "studentId",
+//                 as: "Student_Marks",
+//             },
+//         },       
+//         {   
+//             $unwind: "$Student_Marks",
+//         }, 
+//     ])
+//     .then((result) => {
+//         res.status(200).json({
+//             message:'Student Name and Marks',
+//             result
+//         });
+//         })
+//         .catch((error) => {
+//         console.log(error);
+//         });
+// })
+
+
+// router.get('/greater',async(req,res,next) =>{
+//     Marks.aggregate([
+//         {
+//            $group : {_id:{english : "$english", math: "$math", science : "$science"},
+//             totalMarks : { $sum : "$english", $sum : "$math", $sum : "$science"}}
+//         },
+//         {
+//             $match : {totalMarks : {$gte: 30}}
+//         },
+//     ])
+//     .then((result) => {
+//         res.status(200).json({
+//             message:'Student Marks greather than 30',
+//             result
+//         });
+//         })
+//         .catch((error) => {
+//         console.log(error);
+//         });
+// })
+
+
 
 
 
